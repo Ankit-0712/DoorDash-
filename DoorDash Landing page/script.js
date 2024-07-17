b178e2c7261842bcb734755504866482e7917394
Ankit-0712 <159069425+Ankit-0712@users.noreply.github.com>
2024-07-17 13:08:45 +0530

    Add files via upload

---
+++ DoorDash Landing page/script.js
@@ -0,0 +1,7 @@
+document.getElementById('signInBtn').addEventListener('click', function() {
+    alert('Sign In button clicked');
+});
+
+document.getElementById('signUpBtn').addEventListener('click', function() {
+    alert('Sign Up button clicked');
+});
